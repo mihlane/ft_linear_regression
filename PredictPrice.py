03d32ec6fd4ee4757ab050571af13b5f7bf68876
Mohammed Habibi ihlane <mhabibi-@c4r4p6.1337.ma>
2025-05-31 15:32:04 +0100

    predict

--- PredictPrice.py
+++ PredictPrice.py
@@ -3,3 +3,13 @@ def estimated_price(mileage):
     est_price  = theta0 + (theta1 * mileage)
     return est_price
 
+def main():
+    value = input("> Please entre your mileage: ")
+    if type(value) != float or type(value) != int:
+        print("You should pass int or float")
+        exit()
+    else:
+        print("good")
+
+if __name__ == "__main__":
+    main()
